1d515bba65edb3354eab3b22e65f162bf92477f0
Sumanvith <dellbilla@gmail.com>
2026-06-27 21:56:01 +0530

    get and post of db results

---
+++ src/tasks/controller.py
@@ -0,0 +1,18 @@
+from src.tasks.dtos import TaskSchema
+from sqlalchemy.orm import Session
+from src.tasks.models import TaskModel
+
+
+def create_task(body: TaskSchema, db: Session):
+    data = body.model_dump()
+    new_task = TaskModel(
+        title=data['title'], description=data['description'], is_completed=data['is_completed'])
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+    return {"Status": "Task created", "data": new_task}
+
+
+def get_tasks(db: Session):
+    tasks = db.query(TaskModel).all()
+    return {"data": tasks}
