86e4396caf998ad4eb363c1fa6fe55544d4e200c
Sumanvith <dellbilla@gmail.com>
2026-06-29 22:26:51 +0530

    Revert "one task, update and delete task"
    This reverts commit b17c8c2044793dd6d6ccb779db7abda3e723e321.

--- src/tasks/controller.py
+++ src/tasks/controller.py
@@ -20,6 +20,7 @@ def create_task(body: TaskSchema, db: Session):
 def get_tasks(db: Session):
     tasks = db.query(TaskModel).all()
     return {"data": tasks}
+<<<<<<< HEAD
 
 
 def get_one_task(task_id: int, db: Session):
@@ -49,3 +50,5 @@ def delete_task(task_id: int, db: Session):
     db.delete(one_task)
     db.commit()
     return {"status": "task deleted"}
+=======
+>>>>>>> 1d515bba65edb3354eab3b22e65f162bf92477f0
